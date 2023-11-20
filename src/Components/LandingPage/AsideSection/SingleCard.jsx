7cab1b17ce4009509a90f47895bc49fcf084ec63
Marco Pugliese <126952052+Marco-Pugliese@users.noreply.github.com>
2023-11-20 16:16:04 +0100

    littleUpdate

--- src/Components/LandingPage/AsideSection/SingleCard.jsx
+++ src/Components/LandingPage/AsideSection/SingleCard.jsx
@@ -1,22 +1,17 @@
 import { Card } from "react-bootstrap";
-import { ArrowRightSquare } from "react-bootstrap-icons";
+import { ArrowRightSquare, ArrowRightSquareFill } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
 
 const SingleCard = () => {
   return (
     <Card className="mx-2 shadow-sm">
       <Card.Img variant="top" src="https://placekitten.com/300/300" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint
-          culpa itaque ut illum nobis quo, accusantium, blanditiis beatae,
-          eveniet nulla quasi repellendus. Autem veritatis tempora eum
-          reiciendis consequuntur necessitatibus?
-        </Card.Text>
-        <div className="text-end">
+      <Card.Body className="p-0 mx-0 text-center">
+        <Card.Title className="smaller">Card Title</Card.Title>
+        <Card.Text className="smaller number">Card Description</Card.Text>
+        <div className="text-end me-2 mb-1">
           <Link to="/articles">
-            <ArrowRightSquare />
+            <ArrowRightSquareFill className="fs-5 verdeMain" />
           </Link>
         </div>
       </Card.Body>
